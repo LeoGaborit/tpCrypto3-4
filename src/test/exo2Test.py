3e5f4579c553528bc7da6d49e4e6d4e6fccadb35
Léo Gaborit <leo.gaborit@etu.unilim.fr>
2024-10-18 16:38:19 +0200

    final commit

--- src/test/exo2Test.py
+++ src/test/exo2Test.py
@@ -6,15 +6,16 @@ Chemin vers le script principal : src/main/exo2.py
 """
 
 from src.main.exo2 import genererMDPtailleN
+def test_exo2():
+    # Test avec des entrées valides (4 + 4 => 8)
+    print(genererMDPtailleN("hello", "world", 12)) # 12 caractères
+    assert len(genererMDPtailleN("hello", "world", 12)) == 12
 
-print(genererMDPtailleN("hello", "world", 12)) # 12 caractères
-assert len(genererMDPtailleN("hello", "world", 12)) == 12
+    print(genererMDPtailleN("test", "mdp", 1)) # 1 caractère
+    assert len(genererMDPtailleN("test", "mdp", 1)) == 1
 
-print(genererMDPtailleN("test", "mdp", 1)) # 1 caractère
-assert len(genererMDPtailleN("test", "mdp", 1)) == 1
+    print(genererMDPtailleN("essai", "password", 8)) # 8 caractères
+    assert len(genererMDPtailleN("essai", "password", 8)) == 8
 
-print(genererMDPtailleN("essai", "password", 8)) # 8 caractères
-assert len(genererMDPtailleN("essai", "password", 8)) == 8
-
-print(genererMDPtailleN("leo", "florian", 4)) # 4 caractères
-assert len(genererMDPtailleN("leo", "florian", 4)) == 4
+    print(genererMDPtailleN("leo", "florian", 4)) # 4 caractères
+    assert len(genererMDPtailleN("leo", "florian", 4)) == 4
--- src/test/exo3Test.py
+++ src/test/exo3Test.py
@@ -4,34 +4,31 @@ Tests pour le fichier exo3.py
 
 Chemin vers le script principal : src/main/exo3.py
 """
-
-#Création des tests
-
 from src.main.exo3 import genererMDPcleMaitre
 
 def test_genererMDPtailleN():
     #Test avec une taille de 1
     assert len(genererMDPcleMaitre("hello", 1)) == 1
     #Test avec une taille de 2
-    assert len(genererMDPcleMaitre("hello", 2)) == 2
+    assert len(genererMDPcleMaitre("olleh", 2)) == 2
     #Test avec une taille de 3
-    assert len(genererMDPcleMaitre("hello", 3)) == 3
+    assert len(genererMDPcleMaitre("test", 3)) == 3
     #Test avec une taille de 4
-    assert len(genererMDPcleMaitre("hello", 4)) == 4
+    assert len(genererMDPcleMaitre("mdp", 4)) == 4
     #Test avec une taille de 5
-    assert len(genererMDPcleMaitre("hello", 5)) == 5
+    assert len(genererMDPcleMaitre("bonjour", 5)) == 5
     #Test avec une taille de 6
-    assert len(genererMDPcleMaitre("hello", 6)) == 6
+    assert len(genererMDPcleMaitre("noix", 6)) == 6
     #Test avec une taille de 7
-    assert len(genererMDPcleMaitre("hello", 7)) == 7
+    assert len(genererMDPcleMaitre("igloo", 7)) == 7
     #Test avec une taille de 8
-    assert len(genererMDPcleMaitre("hello", 8)) == 8
+    assert len(genererMDPcleMaitre("gingembre", 8)) == 8
     #Test avec une taille de 9
-    assert len(genererMDPcleMaitre("hello", 9)) == 9
+    assert len(genererMDPcleMaitre("gourde", 9)) == 9
     #Test avec une taille de 10
-    assert len(genererMDPcleMaitre("hello", 10)) == 10
+    assert len(genererMDPcleMaitre("ere", 10)) == 10
     #Test avec une taille de 11
-    assert len(genererMDPcleMaitre("hello", 11)) == 11
+    assert len(genererMDPcleMaitre("reflexe", 11)) == 11
     #Test avec une taille de 12
-    assert len(genererMDPcleMaitre("hello", 12)) == 12
+    assert len(genererMDPcleMaitre("salutation", 12)) == 12
 
